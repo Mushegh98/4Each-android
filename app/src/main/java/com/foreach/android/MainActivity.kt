1d14682057df2d0f0c78276e14e104cbcf17fec3
Mushegh98 <musheghsahakyan98@gmail.com>
2021-03-12 20:38:33 +0400

    Add Project in git

---
+++ app/src/main/java/com/foreach/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.foreach.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
